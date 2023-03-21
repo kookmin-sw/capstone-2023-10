9309860e3aa452926f4d19325de535cfe82a8a59
kh3654po <kh3654po@gmail.com>
2023-03-21 20:59:32 +0900

    메인 워커 시스템에 스팟인스턴스를 생성하고 종료하는 기능, 생성된 인스턴스의 상태를 볼 수 있는 기능 추가

--- main_worker_dns.py
+++ main_worker_dns.py
@@ -1,7 +1,12 @@
 import boto3
 
-region_name = 'ap-northeast-2'  # seoul
+### 임시코드
+import time
+
+###
+
 profile_name = 'kmubigdata'
+region_name = 'us-west-2'
 
 session = boto3.session.Session(profile_name=profile_name, region_name=region_name)
 ec2_client = session.client('ec2')
@@ -24,3 +29,76 @@ def terminate_instances(instance_ids):
 
     return
 
+
+def request_spot_instances(image_id, instance_type, key_name, az=None, instance_count=1, user_data=''):
+    if az is None:
+        new_instances = ec2_client.request_spot_instances(InstanceCount=instance_count,
+                                                          LaunchSpecification={
+                                                              'ImageId': image_id,
+                                                              'InstanceType': instance_type,
+                                                              'KeyName': key_name,
+                                                              'UserData': user_data,
+                                                          })
+    else:
+        new_instances = ec2_client.request_spot_instances(InstanceCount=instance_count,
+                                                          LaunchSpecification={
+                                                              'ImageId': image_id,
+                                                              'InstanceType': instance_type,
+                                                              'KeyName': key_name,
+                                                              'UserData': user_data,
+                                                              "Placement": {
+                                                                  "AvailabilityZone": az
+                                                              },
+                                                          })
+
+    return new_instances
+
+
+def cancel_spot_instance_requests(spot_instance_request_ids):
+    if type(spot_instance_request_ids) == list:
+        ec2_client.cancel_spot_instance_requests(SpotInstanceRequestIds=spot_instance_request_ids)
+    elif type(spot_instance_request_ids) == str:
+        ec2_client.cancel_spot_instance_requests(SpotInstanceRequestIds=[spot_instance_request_ids])
+    else:
+        raise Exception("spot_instance_ids must be \'str\' or \'list\'")
+
+
+def get_spot_instance_request_ids(spot_instances):
+    spot_instance_request_ids = spot_instances.get('SpotInstanceRequests')
+
+    ids = []
+    for spot_instance in spot_instance_request_ids:
+        spot_instance_request_id = spot_instance.get('SpotInstanceRequestId')
+        ids.append(spot_instance_request_id)
+
+    return ids
+
+
+def describe_spot_instance_requests(spot_instance_request_ids):
+    status = ec2_client.describe_spot_instance_requests(SpotInstanceRequestIds=spot_instance_request_ids)
+
+    return status
+
+
+
+### test codes
+# create_instances('t2.medium', 'ami-0ee93c90bc65c86c2', 'kh-oregon')
+# terminate_instances(['i-06569c50a3236d0a9'])
+#
+new_instances = request_spot_instances('ami-016b1f9568b08fffb', 'a1.medium', 'kh-oregon', 1, f'{region_name}c')
+
+spot_instance_request_ids = get_spot_instance_request_ids(new_instances)
+print(f'request spot instance ids : {spot_instance_request_ids}')
+print(new_instances)
+
+### 임시코드
+time.sleep(5)
+status = describe_spot_instance_requests(spot_instance_request_ids)
+print(status)
+
+time.sleep(15)
+###
+
+
+cancel_spot_instance_requests(spot_instance_request_ids)
+print(f'canceled spot instance ids : {spot_instance_request_ids}')
