2fa9fc004e07908479a45cb6fa3d6de69497d1c3
chris0765 <jaeil@kookmin.ac.kr>
2023-04-10 15:05:16 +0900

    Add code about getting red hat ami with instance type's architecture

--- system/migration.py
+++ system/migration.py
@@ -4,19 +4,61 @@ import azure.core
 import google.cloud
 # etc
 
-# Set Default Variance
+# ==================================================================================
+# ============================== Set Default Variance ==============================
+# ==================================================================================
+# << AWS >>
+# Your Account Profile in Your AWS-CLI
+AWS_PROFILE_NAME = "spotrank"
+AWS_REGION_NAME = "us-west-2"
+# Your Account's Red Hat AMI
+AMI_ARM = "ami-08911268ee09cb08e"
+AMI_INTEL = "ami-0dda7e535b65b6469"
+# << GCP >>
 # ...
+# << AZURE >>
+# ...
+# ==================================================================================
+
+
+def arch_to_ami(instanceType, ec2_client):
+    archs = ec2_client.describe_instance_types(InstanceTypes=[instanceType])['InstanceTypes'][0]['ProcessorInfo']['SupportedArchitectures']
+    print(archs)
+    if 'arm64' in archs or 'arm64_mac' in archs:
+        return AMI_ARM
+    else:
+        return AMI_INTEL
 
 
 # Make New Instance to Migrate
 def new_instance(instanceInfo, subnetInfo):
     vendor = instanceInfo['Vendor']
-    instanceType = instanceInfo['InstanceType']
 
     requestResponse = {"Vendor" : vendor, "Response" : None}
     try:
         if vendor == 'AWS':
             # Request AWS Spot Instance in AWS Subnet
+            instanceType = instanceInfo['InstanceType']
+            az = instanceInfo['AZ']
+            region = instanceInfo['Region']
+
+            session = boto3.session.Session(profile_name=AWS_PROFILE_NAME, region_name=AWS_REGION_NAME)
+            ec2_client = session.client('ec2')
+            waiter = ec2_client.get_waiter('spot_instance_request_fulfilled')
+            imageId = arch_to_ami(instanceType, ec2_client)
+            print(imageId)
+
+            # SubnetId = subnetInfo['SubnetId']
+            # requestResponse['Response'] = ec2_client.request_spot_instances(
+            #     InstanceCount=1,
+            #     LaunchSpecification={
+            #         'ImageId': imageId,
+            #         'InstanceType':instanceType,
+            #         'Placement': {'AvailabilityZone':az},
+            #         'SubnetId':subnetId,
+            #         'Userdata':''
+            #     }
+            # )
             pass
         elif vendor == 'AZURE':
             # Request AZURE Spot VM in AZURE Subnet
