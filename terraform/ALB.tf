b70311aebfce70188269287866c9a11f20a2a544
kh3654po <kh3654po@gmail.com>
2023-04-28 10:54:01 +0900

    각 tf 파일의 코드 정렬 및 alb tg, alb sg 포트번호 변경

--- terraform/ALB.tf
+++ terraform/ALB.tf
@@ -1,37 +1,48 @@
 resource "aws_security_group" "alb-sg" {
   vpc_id = aws_vpc.vpc.id
-  name = "${var.prefix}-alb-sg"
-  egress = [ {
-    cidr_blocks = [ "0.0.0.0/0" ]
-    from_port = 0
-    protocol = -1
-    to_port = 0
+  name   = "${var.prefix}-alb-sg"
+  egress = [{
+    cidr_blocks      = ["0.0.0.0/0"]
+    from_port        = 0
+    protocol         = -1
+    to_port          = 0
     description      = "sg"
     ipv6_cidr_blocks = []
     prefix_list_ids  = []
     security_groups  = []
     self             = false
-  } ]
-  ingress = [ {
-    cidr_blocks = [ "0.0.0.0/0" ]
-    from_port = 80
-    protocol = "tcp"
-    to_port = 80
+  }]
+  ingress = [{
+    cidr_blocks      = ["0.0.0.0/0"]
+    from_port        = 80
+    protocol         = "tcp"
+    to_port          = 80
     description      = "sg"
     ipv6_cidr_blocks = []
     prefix_list_ids  = []
     security_groups  = []
     self             = false
-  } ]
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      from_port        = 8800
+      protocol         = "tcp"
+      to_port          = 8800
+      description      = "sg"
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+  }]
 }
 
 resource "aws_lb" "alb" {
-  subnets = [ aws_subnet.subnet.id, aws_subnet.subnet2.id ]
-  internal = false
-  name = "${var.prefix}-alb"
+  subnets                    = [aws_subnet.subnet.id, aws_subnet.subnet2.id]
+  internal                   = false
+  name                       = "${var.prefix}-alb"
   enable_deletion_protection = false
-  security_groups = [ aws_security_group.alb-sg.id ]
-  load_balancer_type = "application"
+  security_groups            = [aws_security_group.alb-sg.id]
+  load_balancer_type         = "application"
 }
 
 resource "aws_lb_target_group" "main-worker-tg" {
@@ -41,23 +52,17 @@ resource "aws_lb_target_group" "main-worker-tg" {
   vpc_id   = aws_vpc.vpc.id
 }
 
-# resource "aws_lb_target_group_attachment" "main-worker-tg-attachment" {
-#   target_group_arn = aws_lb_target_group.main-worker-tg.arn
-#   target_id = aws_lambda_function.main-worker.id
-#   port = 80
-# }
-
 resource "aws_lb_target_group" "jupyter-tg" {
   name     = "jupyter-tg"
-  port     = 80
+  port     = 8800
   protocol = "HTTP"
   vpc_id   = aws_vpc.vpc.id
 }
 
 resource "aws_lb_listener" "main-worker-listener" {
   load_balancer_arn = aws_lb.alb.arn
-  port = "80"
-  protocol = "HTTP"
+  port              = "80"
+  protocol          = "HTTP"
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.main-worker-tg.arn
